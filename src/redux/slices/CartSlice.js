56777dec396712cbcb8dc2510f6ae42b334a787c
AyushThakur34 <thakurayush9887@gmail.com>
2025-06-15 22:52:33 +0530

    file structuremanaged

---
+++ src/redux/slices/CartSlice.js
@@ -0,0 +1,16 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+
+};
+
+const CartSlice = createSlice({
+    name: cart,
+    initialState,
+    reducers: {
+
+    }
+})
+
+export const { } = CartSlice.actions;
+export default CartSlice.reducer;
